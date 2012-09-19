fba793b0b5b60949f2423104f798d20ef10b19fa
oakes <zsoakes@gmail.com>
2012-09-18 20:02:45 -0400

    Fix the proxy mechanism

--- nightweb-desktop/src/nightweb/core.clj
+++ nightweb-desktop/src/nightweb/core.clj
@@ -2,18 +2,23 @@
   (:use nightweb.console)
   (:import net.i2p.router.Router
            net.i2p.i2ptunnel.I2PTunnel
-           net.i2p.util.EventDispatcher)
+           java.net.ProxySelector
+           java.net.Proxy
+           java.net.InetSocketAddress)
   (:gen-class))
 
 (defn start-http-proxy
   "Launch the http proxy."
   []
-  (java.lang.System/setProperty "http.proxyHost" "localhost")
-  (java.lang.System/setProperty "http.proxyPort" "4708")
-  (java.lang.System/setProperty "https.proxyHost" "localhost")
-  (java.lang.System/setProperty "https.proxyPort" "4708")
+  (let [proxy-selector (proxy [ProxySelector] []
+                         (select [uri]
+                                 [(Proxy. java.net.Proxy$Type/HTTP
+                                          (InetSocketAddress. "localhost" 4707))])
+                         (connectFailed [uri socket exception]
+                                        (println "Connect failed.")))]
+    (ProxySelector/setDefault proxy-selector))
   (future (I2PTunnel/main
-            (into-array java.lang.String ["-nogui" "-nocli" "-e" "httpclient 4708"]))))
+            (into-array java.lang.String ["-nogui" "-nocli" "-e" "httpclient 4707"]))))
 
 (defn start-router
   "Launch the router."
