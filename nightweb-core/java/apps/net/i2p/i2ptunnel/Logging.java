78341a7a90ea1b38d186666f02455a9ec9e3c85b
oskar <oskar@oskar-VirtualBox.(none)>
2012-10-03 17:27:45 -0400

    Get rid of i2p tunnel

--- nightweb-core/java/apps/net/i2p/i2ptunnel/Logging.java
+++
@@ -1,8 +0,0 @@
-/* I2PTunnel is GPL'ed (with the exception mentioned in I2PTunnel.java)
- * (c) 2003 - 2004 mihi
- */
-package net.i2p.i2ptunnel;
-
-public interface Logging {
-    public void log(String s);
-}
