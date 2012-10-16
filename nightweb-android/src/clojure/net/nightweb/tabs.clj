1556b4f05d2da5ad59d887531d880ac82fe15ae9
oakes <zsoakes@gmail.com>
2012-10-16 02:06:05 -0400

    Rearrange clojure tabs code and add a bit more

--- nightweb-android/src/clojure/net/nightweb/tabs.clj
+++ nightweb-android/src/clojure/net/nightweb/tabs.clj
@@ -13,65 +13,6 @@
 
 (do
   (gen-class
-    :name "net.nightweb.tabs.TabLayout"
-    :extends net.nightweb.LinearLayout
-    :state "state"
-    :init "init"
-    :post-init "post-init"
-    :prefix "tab-layout-"
-    :constructors {[android.content.Context net.nightweb.tabs.TabScrollView] [android.content.Context]}
-    :methods []
-    :exposes-methods {onMeasure superOnMeasure
-                      onLayout superOnLayout})
-  (defn tab-layout-create-state
-    [context tabs]
-    (let [res (.getResources context)
-          tab-overlap (.getDimension res (get-resource :dimen :tab_overlap))]
-      (atom {:tab-overlap tab-overlap
-             :tabs tabs})))
-  (defn tab-layout-init
-    ([context tabs] [[context] (tab-layout-create-state context tabs)]))
-  (defn tab-layout-post-init
-    ([this context]
-      (.superSetChildrenDrawingOrderEnabled this true)
-      )
-    ([this context attrs] (tab-layout-post-init this context))
-    ([this context attrs def-style] (tab-layout-post-init this context)))
-  (defn tab-layout-onMeasure
-    [this hspec vspec]
-    (.superOnMeasure this hspec vspec)
-    (let [child-count (- (.getChildCount this) 1)
-          max (java.lang.Math/max 0 child-count)
-          tab-overlap (get-state this :tab-overlap)]
-      (.superSetMeasuredDimension this
-                                  (- (.getMeasuredWidth this) (* max tab-overlap))
-                                  (.getMeasuredHeight this))
-      ))
-  (defn tab-layout-onLayout
-    [this changed l t r b]
-    (.superOnLayout this changed l t r b)
-    (if (> (.getChildCount this) 0)
-      (for [i (range 1 (.getChildCount this))
-            :let [first-child (.getChildAt this 0)
-                  tab-overlap (get-state this :tab-overlap)
-                  left (- (.getRight first-child) tab-overlap)
-                  total-left (* left i)
-                  tab (.getChildAt this i)
-                  w (- (.getRight tab) (.getLeft tab))]]
-        (.layout tab total-left (.getTop tab) (+ total-left w) (.getBottom tab)))))
-  (defn tab-layout-getChildDrawingOrder
-    [this cnt i]
-    (let [selected (.getSelectedTab (get-state this :tabs))
-          next (- cnt i 1)]
-      (if (and (= i (- cnt 1)) (>= selected 0) (< selected cnt))
-        selected
-        (if (and (<= next selected) (> next 0))
-          (- next 1)
-          next))))
-    )
-
-(do
-  (gen-class
     :name "net.nightweb.tabs.TabScrollView"
     :extends android.widget.HorizontalScrollView
     :state "state"
@@ -83,8 +24,48 @@
                    [android.content.Context android.util.AttributeSet int] [android.content.Context android.util.AttributeSet int]}
     :methods [["addTab" [] void]
               ["setSelectedTab" [android.view.View] void]
-              ["getSelectedTab" [] int]]
+              ["getSelectedTab" [] int]
+              ["animateScroll" [int] void]]
     :exposes-methods {onLayout superOnLayout})
+  (gen-class
+    :name "net.nightweb.tabs.TabLayout"
+    :extends net.nightweb.LinearLayout
+    :state "state"
+    :init "init"
+    :post-init "post-init"
+    :prefix "tab-layout-"
+    :constructors {[android.content.Context net.nightweb.tabs.TabScrollView] [android.content.Context]}
+    :methods []
+    :exposes-methods {onMeasure superOnMeasure
+                      onLayout superOnLayout})
+  (gen-class
+    :name "net.nightweb.tabs.TabView"
+    :extends net.nightweb.LinearLayout
+    :state "state"
+    :init "init"
+    :post-init "post-init"
+    :prefix "tab-view-"
+    :constructors {[android.content.Context] [android.content.Context]
+                   [android.content.Context android.util.AttributeSet] [android.content.Context android.util.AttributeSet]
+                   [android.content.Context android.util.AttributeSet int] [android.content.Context android.util.AttributeSet int]}
+    :methods [["setTitle" [String] void]]
+    :exposes-methods {onLayout superOnLayout
+                      setActivated superSetActivated})
+  (gen-class
+    :name "net.nightweb.tabs.TabBar"
+    :extends net.nightweb.LinearLayout
+    :state "state"
+    :init "init"
+    :post-init "post-init"
+    :prefix "tab-bar-"
+    :constructors {[android.content.Context] [android.content.Context]
+                   [android.content.Context android.util.AttributeSet] [android.content.Context android.util.AttributeSet]
+                   [android.content.Context android.util.AttributeSet int] [android.content.Context android.util.AttributeSet int]}
+    :methods []
+    :exposes-methods {onMeasure superOnMeasure
+                      onConfigurationChanged superOnConfigurationChanged}))
+
+(do
   (defn tab-scroll-view-create-state
     [context]
     (atom {:context context
@@ -97,7 +78,8 @@
     ([this context]
       (let [res (.getResources context)
             tab-first-padding-left (.getDimension res (get-resource :dimen :tab_first_padding_left))
-            content-view (net.nightweb.tabs.TabLayout. context this)]
+            content-view (net.nightweb.tabs.TabLayout. context this)
+            animation-duration (.getInteger res (get-resource :integer :tab_animation_duration))]
         (.setHorizontalScrollBarEnabled this false)
         (.setOverScrollMode this android.view.View/OVER_SCROLL_NEVER)
         (.setOrientation content-view android.widget.LinearLayout/HORIZONTAL)
@@ -108,14 +90,23 @@
         (.setPadding content-view tab-first-padding-left 0 0 0)
         (.addView this content-view)
         (.scrollTo this (.getScrollX this) (.getScrollY this))
-        (set-state! this :content-view content-view)))
+        (set-state! this :content-view content-view)
+        (set-state! this :animation-duration animation-duration)))
     ([this context attrs] (tab-scroll-view-post-init this context))
     ([this context attrs def-style] (tab-scroll-view-post-init this context)))
   (defn tab-scroll-view-onLayout
     [this changed l t r b]
     (.superOnLayout this changed l t r b)
-    (let []
-      ))
+    (if-let [child (.getChildAt (get-state this :content-view)
+                                (get-state this :selected))]
+      (let [childl (.getLeft child)
+            childr (+ childl (.getWidth child))
+            viewl (.getScrollX this)
+            viewr (+ viewl (.getWidth this))]
+        (if (< childl viewl)
+          (.animateScroll this childl)
+          (if (> childr viewr)
+            (.animateScroll this (+ (- childr viewr) viewl)))))))
   (defn tab-scroll-view-addTab
     [this]
     (let [content-view (get-state this :content-view)
@@ -140,22 +131,61 @@
   (defn tab-scroll-view-getSelectedTab
     [this]
     (get-state this :selected))
+  (defn tab-scroll-view-animateScroll
+    [this new-scroll]
+    (let [animator (android.animation.ObjectAnimator/ofInt
+                     this "scroll" (.getScrollX this))]
+      (.setDuration animator (get-state this :animation-duration))
+      (.start animator)))
   )
 
 (do
-  (gen-class
-    :name "net.nightweb.tabs.TabView"
-    :extends net.nightweb.LinearLayout
-    :state "state"
-    :init "init"
-    :post-init "post-init"
-    :prefix "tab-view-"
-    :constructors {[android.content.Context] [android.content.Context]
-                   [android.content.Context android.util.AttributeSet] [android.content.Context android.util.AttributeSet]
-                   [android.content.Context android.util.AttributeSet int] [android.content.Context android.util.AttributeSet int]}
-    :methods [["setTitle" [String] void]]
-    :exposes-methods {onLayout superOnLayout
-                      setActivated superSetActivated})
+  (defn tab-layout-create-state
+    [context tabs]
+    (let [res (.getResources context)
+          tab-overlap (.getDimension res (get-resource :dimen :tab_overlap))]
+      (atom {:tab-overlap tab-overlap
+             :tabs tabs})))
+  (defn tab-layout-init
+    ([context tabs] [[context] (tab-layout-create-state context tabs)]))
+  (defn tab-layout-post-init
+    ([this context]
+      (.superSetChildrenDrawingOrderEnabled this true))
+    ([this context attrs] (tab-layout-post-init this context))
+    ([this context attrs def-style] (tab-layout-post-init this context)))
+  (defn tab-layout-onMeasure
+    [this hspec vspec]
+    (.superOnMeasure this hspec vspec)
+    (let [child-count (- (.getChildCount this) 1)
+          max (java.lang.Math/max 0 child-count)
+          tab-overlap (get-state this :tab-overlap)]
+      (.superSetMeasuredDimension this
+                                  (- (.getMeasuredWidth this) (* max tab-overlap))
+                                  (.getMeasuredHeight this))))
+  (defn tab-layout-onLayout
+    [this changed l t r b]
+    (.superOnLayout this changed l t r b)
+    (if (> (.getChildCount this) 0)
+      (for [i (range 1 (.getChildCount this))
+            :let [first-child (.getChildAt this 0)
+                  tab-overlap (get-state this :tab-overlap)
+                  left (- (.getRight first-child) tab-overlap)
+                  total-left (* left i)
+                  tab (.getChildAt this i)
+                  w (- (.getRight tab) (.getLeft tab))]]
+        (.layout tab total-left (.getTop tab) (+ total-left w) (.getBottom tab)))))
+  (defn tab-layout-getChildDrawingOrder
+    [this cnt i]
+    (let [selected (.getSelectedTab (get-state this :tabs))
+          next (- cnt i 1)]
+      (if (and (= i (- cnt 1)) (>= selected 0) (< selected cnt))
+        selected
+        (if (and (<= next selected) (> next 0))
+          (- next 1)
+          next))))
+    )
+
+(do
   (defn tab-view-create-state
     [context]
     (atom {:context context
@@ -176,13 +206,16 @@
       (.setPadding this
                    (get-state this :tab-overlap) 0
                    (get-state this :tab-slice-width) 0)
-      (let [inflater (android.view.LayoutInflater/from context)
+      (let [res (.getResources context)
+            tab-width (.getDimension res (get-resource :dimen :tab_width))
+            inflater (android.view.LayoutInflater/from context)
             tab-content (.inflate inflater (get-resource :layout :tab_title) this true)
             title (find-view tab-content (get-resource :id :title))
             icon-view (find-view tab-content (get-resource :id :favicon))
             lock (find-view tab-content (get-resource :id :lock))
             close (find-view tab-content (get-resource :id :close))]
         (.setOnClickListener close (on-click (println "close")))
+        (set-state! this :tab-width tab-width)
         (set-state! this :tab-content tab-content)
         (set-state! this :title title)
         (set-state! this :icon-view icon-view)
@@ -217,15 +250,16 @@
     (let [context (get-state this :context)
           title (get-state this :title)
           icon-view (get-state this :icon-view)
-          close (get-state this :close)]
+          close (get-state this :close)
+          lp (.getLayoutParams this)]
       (.setTextAppearance title context (if selected (get-resource :style :TabTitleSelected) (get-resource :style :TabTitleUnselected)))
       (.setVisibility icon-view (if selected android.view.View/GONE android.view.View/VISIBLE))
       (.setVisibility close (if selected android.view.View/VISIBLE android.view.View/GONE))
       (.setHorizontalFadingEdgeEnabled this (not selected))
       (.superSetActivated this selected)
-;      (.setLayoutParams this (android.view.ViewGroup$LayoutParams.
-;                               (get-state this :tab-width)
-;                               android.view.ViewGroup$LayoutParams/MATCH_PARENT))
+;      (set! (. lp width) (get-state this :tab-width))
+;      (set! (. lp height) android.view.ViewGroup$LayoutParams/MATCH_PARENT)
+;      (.setLayoutParams this lp)
       (.setFocusable this (not selected))
       (.postInvalidate this)))
   (defn tab-view-setTitle
@@ -235,25 +269,11 @@
   )
 
 (do
-  (gen-class
-    :name "net.nightweb.tabs.TabBar"
-    :extends net.nightweb.LinearLayout
-    :state "state"
-    :init "init"
-    :post-init "post-init"
-    :prefix "tab-bar-"
-    :constructors {[android.content.Context] [android.content.Context]
-                   [android.content.Context android.util.AttributeSet] [android.content.Context android.util.AttributeSet]
-                   [android.content.Context android.util.AttributeSet int] [android.content.Context android.util.AttributeSet int]}
-    :methods []
-    :exposes-methods {onMeasure superOnMeasure
-                      onConfigurationChanged superOnConfigurationChanged})
   (defn tab-bar-create-state
     [context]
     (let [res (.getResources context)]
       (atom {:context context
              :tab-add-overlap (.getDimension res (get-resource :dimen :tab_addoverlap))
-             :tab-width (.getDimension res (get-resource :dimen :tab_width))
              :tab-map {}})))
   (defn tab-bar-init
     ([context] [[context] (tab-bar-create-state context)])
@@ -276,11 +296,7 @@
   (defn tab-bar-onConfigurationChanged
     [this new-config]
     (.superOnConfigurationChanged this new-config)
-    (let [context (get-state this :context)
-          res (.getResources context)
-          tab-width (get-resource :dimen :tab_width)]
-      (set-state! this :tab-width (.getDimension res tab-width))
-      (.updateLayout (get-state this :tabs))))
+    (.updateLayout (get-state this :tabs)))
   (defn tab-bar-onMeasure
     [this hspec vspec]
     (.superOnMeasure this hspec vspec)
