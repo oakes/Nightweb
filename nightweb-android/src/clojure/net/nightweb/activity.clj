a4f7a4ed3a5b4f927118cd9cc127abbdb361ac7c
oakes <zsoakes@gmail.com>
2012-10-24 21:13:25 -0400

    Add tabs and buttons to the action bar

---
+++ nightweb-android/src/clojure/net/nightweb/activity.clj
@@ -0,0 +1,68 @@
+(ns net.nightweb.activity
+  (:import android.app.Activity)
+  (:use neko.-utils))
+
+(defmacro defactivity
+  "Creates an activity with the given full package-qualified name.
+  Optional arguments should be provided in a key-value fashion.
+
+  Available optional arguments:
+
+  :extends, :prefix - same as for `gen-class`.
+
+  :def - symbol to bind the Activity object to in the onCreate
+  method. Relevant only if :create is used.
+
+  :on-create - takes a two-argument function. Generates a handler for
+  activity's `onCreate` event which automatically calls the
+  superOnCreate method and creates a var with the name denoted by
+  `:def` (or activity's lower-cased name by default) to store the
+  activity object. Then calls the provided function onto the
+  Application object.
+
+  :on-start, :on-restart, :on-resume, :on-pause, :on-stop, :on-destroy
+  - same as :on-create but require a one-argument function."
+  [name & {:keys [extends prefix on-create menu-resource def] :as options}]
+  (let [options (or options {}) ;; Handle no-options case
+        sname (simple-name name)
+        prefix (or prefix (str sname "-"))
+        def (or def (symbol (unicaseize sname)))]
+    `(do
+       (gen-class
+        :name ~name
+        :main false
+        :prefix ~prefix
+        :extends ~(or extends Activity)
+        :exposes-methods {~'onCreate ~'superOnCreate
+                          ~'onStart ~'superOnStart
+                          ~'onRestart ~'superOnRestart
+                          ~'onResume ~'superOnResume
+                          ~'onPause ~'superOnPause
+                          ~'onStop ~'superOnStop
+                          ~'onDestroy ~'superOnDestroy
+                          ~'onCreateOptionsMenu ~'superOnCreateOptionsMenu
+                          ~'getMenuInflater ~'superGetMenuInflater})
+       ~(when on-create
+          `(defn ~(symbol (str prefix "onCreate"))
+             [~(vary-meta 'this assoc :tag name),
+              ^android.os.Bundle ~'savedInstanceState]
+             (.superOnCreate ~'this ~'savedInstanceState)
+             (def ~(vary-meta def assoc :tag name) ~'this)
+             (~on-create ~'this ~'savedInstanceState)))
+       ~(when menu-resource
+          `(defn ~(symbol (str prefix "onCreateOptionsMenu"))
+             [~(vary-meta 'this assoc :tag name),
+              ^android.view.Menu ~'menu]
+             (.superOnCreateOptionsMenu ~'this ~'menu)
+             (def ~(vary-meta def assoc :tag name) ~'this)
+             (.inflate (.superGetMenuInflater ~'this) ~menu-resource ~'menu)
+             true))
+       ~@(map #(let [func (options %)
+                     event-name (keyword->camelcase %)]
+                 (when func
+                   `(defn ~(symbol (str prefix event-name))
+                      [~(vary-meta 'this assoc :tag name)]
+                      (~(symbol (str ".super" (capitalize event-name))) ~'this)
+                      (~func ~'this))))
+              [:on-start :on-restart :on-resume
+               :on-pause :on-stop :on-destroy]))))
--- nightweb-android/src/clojure/net/nightweb/main.clj
+++ nightweb-android/src/clojure/net/nightweb/main.clj
@@ -1,14 +1,22 @@
 (ns net.nightweb.main
-  (:use [neko.activity :only [defactivity set-content-view!]]
+  (:use [neko.activity :only [set-content-view!]]
         [neko.threading :only [on-ui]]
         [neko.ui :only [make-ui]]
         [neko.application :only [defapplication]]
         [neko.notify :only [notification fire]]
         [neko.resource :only [get-resource]]
-        net.nightweb.service)
+        net.nightweb.service
+        net.nightweb.activity)
   (:require nightweb.router))
 
 (defapplication net.nightweb.Application)
+
+(defn create-tab
+  []
+  (proxy [android.app.ActionBar$TabListener] []
+    (onTabSelected [tab ft])
+    (onTabUnselected [tab ft])
+    (onTabReselected [tab ft])))
 
 (defactivity net.nightweb.MainActivity
   :def a
@@ -20,10 +28,35 @@
                 [:text-view {:text "Hello from Clojure!"}]])))
     (def conn (bind-service this
                             "net.nightweb.MainService"
-                            (fn [service] (.act service "Action!")))))
+                            (fn [service] (.act service "Action!"))))
+    (let [action-bar (.getActionBar this)
+          home-tab (.newTab action-bar)
+          people-tab (.newTab action-bar)
+          photos-tab (.newTab action-bar)
+          audio-tab (.newTab action-bar)
+          videos-tab (.newTab action-bar)]
+      (.setNavigationMode action-bar android.app.ActionBar/NAVIGATION_MODE_TABS)
+      (.setDisplayShowTitleEnabled action-bar false)
+      (.setText home-tab (get-resource :string :home))
+      (.setText people-tab (get-resource :string :people))
+      (.setText photos-tab (get-resource :string :photos))
+      (.setText audio-tab (get-resource :string :audio))
+      (.setText videos-tab (get-resource :string :videos))
+      (.setTabListener home-tab (create-tab))
+      (.setTabListener people-tab (create-tab))
+      (.setTabListener photos-tab (create-tab))
+      (.setTabListener audio-tab (create-tab))
+      (.setTabListener videos-tab (create-tab))
+      (.addTab action-bar home-tab)
+      (.addTab action-bar people-tab)
+      (.addTab action-bar photos-tab)
+      (.addTab action-bar audio-tab)
+      (.addTab action-bar videos-tab)))
   :on-destroy
   (fn [this]
-    (unbind-service this conn)))
+    (unbind-service this conn))
+  :menu-resource
+  (get-resource :menu :main_activity))
 
 (defservice net.nightweb.MainService
   :def s
