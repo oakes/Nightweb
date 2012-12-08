41e5db87ac5e2354ae21c5fba4ea5bc1bba44bce
oakes <zsoakes@gmail.com>
2012-12-07 20:53:08 -0500

    Add action to search button

--- nightweb-android/src/clojure/net/nightweb/menus.clj
+++ nightweb-android/src/clojure/net/nightweb/menus.clj
@@ -5,27 +5,34 @@
 
 (defn create-main-menu
   [context menu]
-  (let [new-post-item (.add menu (get-string :new_post))
-        search-item (.add menu (get-string :search))
+  (let [search-item (.add menu (get-string :search))
+        new-post-item (.add menu (get-string :new_post))
         share-item (.add menu (get-string :share))
-        if-room android.view.MenuItem/SHOW_AS_ACTION_IF_ROOM]
-    (.setIcon new-post-item (get-resource :drawable :android/ic_menu_add))
+        if-room android.view.MenuItem/SHOW_AS_ACTION_IF_ROOM
+        collapse-action-view
+        android.view.MenuItem/SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW
+        search-view (android.widget.SearchView. context)]
     (.setIcon search-item (get-resource :drawable :android/ic_menu_search))
+    (.setIcon new-post-item (get-resource :drawable :android/ic_menu_add))
     (.setIcon share-item (get-resource :drawable :android/ic_menu_share))
+    (.setShowAsAction search-item (bit-or if-room collapse-action-view))
     (.setShowAsAction new-post-item if-room)
-    (.setShowAsAction search-item if-room)
     (.setShowAsAction share-item if-room)
+    (.setOnQueryTextListener
+      search-view
+      (proxy [android.widget.SearchView$OnQueryTextListener] []
+        (onQueryTextChange [new-text]
+          (println "typing:" new-text)
+          true)
+        (onQueryTextSubmit [query]
+          (println "submitted:" query)
+          true)))
+    (.setActionView search-item search-view)
     (.setOnMenuItemClickListener
       new-post-item
       (proxy [android.view.MenuItem$OnMenuItemClickListener] []
         (onMenuItemClick [menu-item]
           (show-page context "net.nightweb.NewPostPage")
-          true)))
-    (.setOnMenuItemClickListener
-      search-item
-      (proxy [android.view.MenuItem$OnMenuItemClickListener] []
-        (onMenuItemClick [menu-item]
-          (println "search")
           true)))
     (.setOnMenuItemClickListener
       share-item
