0d80dff6d094549c84ca1d2d77d6c6234cb943ee
oakes <zsoakes@gmail.com>
2013-03-16 01:30:29 -0400

    Fix version number

--- server/project.clj
+++ server/project.clj
@@ -1,4 +1,4 @@
-(defproject nightweb-server/Nightweb "0.1.0-SNAPSHOT"
+(defproject nightweb-server/Nightweb "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Public Domain"
