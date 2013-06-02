cf6c27359bcc16f9ebdf3324f18173e137c92b40
oakes <zsoakes@gmail.com>
2013-06-02 13:42:11 -0400

    Add modifications to I2PSnark

--- common/java/apps/org/klomp/snark/I2PSnarkUtil.java
+++ common/java/apps/org/klomp/snark/I2PSnarkUtil.java
@@ -2,6 +2,7 @@ package org.klomp.snark;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -39,6 +40,8 @@ import net.i2p.util.Translate;
 
 import org.klomp.snark.dht.DHT;
 import org.klomp.snark.dht.KRPC;
+import org.klomp.snark.dht.NodeInfo;
+import org.klomp.snark.dht.CustomQueryHandler;
 
 /**
  * I2P specific helpers for I2PSnark
@@ -71,6 +74,10 @@ public class I2PSnarkUtil {
     private boolean _areFilesPublic;
     private List<String> _openTrackers;
     private DHT _dht;
+    private InputStream _myPrivateKeyStream;
+    private NodeInfo _myNodeInfo;
+    private CustomQueryHandler _customQueryHandler;
+    private Runnable _dhtInitCallback;
 
     private static final int EEPGET_CONNECT_TIMEOUT = 45*1000;
     private static final int EEPGET_CONNECT_TIMEOUT_SHORT = 5*1000;
@@ -185,6 +192,19 @@ public class I2PSnarkUtil {
 	_configured = true;
     }
     
+    public void setDHTNode(InputStream privateKeyStream, NodeInfo nodeInfo) {
+        _myPrivateKeyStream = privateKeyStream;
+	_myNodeInfo = nodeInfo;
+    }
+
+    public void setDHTCustomQueryHandler(CustomQueryHandler handler) {
+        _customQueryHandler = handler;
+    }
+
+    public void setDHTInitCallback(Runnable callback) {
+        _dhtInitCallback = callback;
+    }
+
     public String getI2CPHost() { return _i2cpHost; }
     public int getI2CPPort() { return _i2cpPort; }
     public Map<String, String> getI2CPOptions() { return _opts; }
@@ -259,11 +279,19 @@ public class I2PSnarkUtil {
                 opts.setProperty("i2p.streaming.enforceProtocol", "true");
             if (opts.getProperty("i2p.streaming.disableRejectLogging") == null)
                 opts.setProperty("i2p.streaming.disableRejectLogging", "true");
-            _manager = I2PSocketManagerFactory.createManager(_i2cpHost, _i2cpPort, opts);
+            if (_myPrivateKeyStream != null) {
+                _manager = I2PSocketManagerFactory.createManager(_myPrivateKeyStream, _i2cpHost, _i2cpPort, opts);
+            } else {
+                _manager = I2PSocketManagerFactory.createManager(_i2cpHost, _i2cpPort, opts);
+            }
             _connecting = false;
         }
-        if (_shouldUseDHT && _manager != null && _dht == null)
-            _dht = new KRPC(_context, _baseName, _manager.getSession());
+        if (_shouldUseDHT && _manager != null && _dht == null) {
+            _dht = new KRPC(_context, _baseName, _manager.getSession(), _myNodeInfo, _customQueryHandler);
+            if (_dhtInitCallback != null) {
+                _dhtInitCallback.run();
+            }
+	}
         return (_manager != null);
     }
     
@@ -598,7 +626,10 @@ public class I2PSnarkUtil {
     public synchronized void setUseDHT(boolean yes) {
         _shouldUseDHT = yes;
         if (yes && _manager != null && _dht == null) {
-            _dht = new KRPC(_context, _baseName, _manager.getSession());
+            _dht = new KRPC(_context, _baseName, _manager.getSession(), _myNodeInfo, _customQueryHandler);
+            if (_dhtInitCallback != null) {
+                _dhtInitCallback.run();
+            }
         } else if (!yes && _dht != null) {
             _dht.stop();
             _dht = null;
--- common/java/apps/org/klomp/snark/TrackerClient.java
+++ common/java/apps/org/klomp/snark/TrackerClient.java
@@ -85,7 +85,7 @@ public class TrackerClient implements Runnable {
   private final static int MAX_CONSEC_FAILS = 5;    // slow down after this
   private final static int LONG_SLEEP = 30*60*1000; // sleep a while after lots of fails
   private final static long MIN_TRACKER_ANNOUNCE_INTERVAL = 15*60*1000;
-  private final static long MIN_DHT_ANNOUNCE_INTERVAL = 10*60*1000;
+  private final static long MIN_DHT_ANNOUNCE_INTERVAL = 60*1000;
 
   private final I2PSnarkUtil _util;
   private final MetaInfo meta;
@@ -378,6 +378,11 @@ public class TrackerClient implements Runnable {
                 dht.announce(snark.getInfoHash());
 
             int maxSeenPeers = 0;
+            int p = getPeersFromDHT();
+            if (p > maxSeenPeers) {
+                maxSeenPeers = p;
+            }
+/*
             if (!trackers.isEmpty())
                 maxSeenPeers = getPeersFromTrackers(trackers);
             int p = getPeersFromPEX();
@@ -392,6 +397,7 @@ public class TrackerClient implements Runnable {
                 if (p > maxSeenPeers)
                     maxSeenPeers = p;
             }
+*/
 
             // we could try and total the unique peers but that's too hard for now
             snark.setTrackerSeenPeers(maxSeenPeers);
@@ -402,7 +408,8 @@ public class TrackerClient implements Runnable {
             try {
                 // Sleep some minutes...
                 // Sleep the minimum interval for all the trackers, but 60s minimum
-                int delay;
+                int delay = 60000;
+/*
                 Random r = _util.getContext().random();
                 int random = r.nextInt(120*1000);
                 if (completed && runStarted)
@@ -414,6 +421,7 @@ public class TrackerClient implements Runnable {
                 else
                   // sleep a while, when we wake up we will contact only the trackers whose intervals have passed
                   delay = SLEEP*60*1000 + random;
+*/
 
                 if (delay > 20*1000) {
                   // put ourselves on SimpleTimer2
