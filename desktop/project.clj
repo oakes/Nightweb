f15c451debc2c53242a3a4f436f9cd459df9b8f1
oakes <zsoakes@gmail.com>
2013-09-20 14:31:44 -0400

    Update libraries and make :aot explicit

--- desktop/project.clj
+++ desktop/project.clj
@@ -1,16 +1,17 @@
 (defproject nightweb-desktop "0.0.20"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.h2database/h2 "1.3.170"]
-                 [seesaw "1.4.3"]
-                 [com.github.insubstantial/substance "7.1"]
-                 [ring "1.1.8"]
+  :dependencies [[com.github.insubstantial/substance "7.2.1"]
+                 [com.h2database/h2 "1.3.173"]
                  [hiccup "1.0.3"]
-                 [markdown-clj "0.9.19"]]
-  :main nightweb-desktop.core
+                 [markdown-clj "0.9.19"]
+                 [org.clojure/clojure "1.5.1"]
+                 [ring "1.1.8"]
+                 [seesaw "1.4.3"]]
   :source-paths ["src" "../common/clojure"]
   :java-source-paths ["../common/java"]
   :resource-paths ["resources"
                    "../android/res"
-                   "../android/res/drawable"])
+                   "../android/res/drawable"]
+  :aot [nightweb-desktop.core]
+  :main nightweb-desktop.core)
--- server/project.clj
+++ server/project.clj
@@ -1,8 +1,9 @@
 (defproject nightweb-server/Nightweb "0.0.20"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [com.h2database/h2 "1.3.170"]]
-  :main nightweb-server.core
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.h2database/h2 "1.3.173"]]
   :source-paths ["src" "../common/clojure"]
-  :java-source-paths ["../common/java"])
+  :java-source-paths ["../common/java"]
+  :aot [nightweb-server.core]
+  :main nightweb-server.core)
