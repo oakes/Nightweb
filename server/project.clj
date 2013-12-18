962b478e7b1ec0816a58039c081b743554b3df3a
oakes <zsoakes@gmail.com>
2013-12-18 11:43:08 -0500

    Update libraries

--- server/project.clj
+++ server/project.clj
@@ -2,7 +2,7 @@
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.0-beta2"]
+                 [org.clojure/java.jdbc "0.3.0"]
                  [com.h2database/h2 "1.3.174"]]
   :source-paths ["src" "../common/clojure"]
   :java-source-paths ["../common/java"]
