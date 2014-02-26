23f782a5ddf70c894e641943193e347bd99ac0e0
oakes <zsoakes@gmail.com>
2014-02-26 17:44:49 -0500

    Increment version number

--- desktop/project.clj
+++ desktop/project.clj
@@ -1,12 +1,12 @@
-(defproject nightweb-desktop "0.0.25"
+(defproject nightweb-desktop "0.0.26"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[com.github.insubstantial/substance "7.2.1"]
-                 [com.h2database/h2 "1.3.174"]
-                 [hiccup "1.0.4"]
+                 [com.h2database/h2 "1.3.175"]
+                 [hiccup "1.0.5"]
                  [markdown-clj "0.9.36"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.2"]
+                 [org.clojure/java.jdbc "0.3.3"]
                  [ring "1.2.1"]
                  [seesaw "1.4.4"]]
   :source-paths ["src" "../common/clojure"]
